6e61e149c3feb956fb6514f070a1b47b090067ca
iamgeoknight <pkchaudhary265@gmail.com>
2022-08-21 09:34:51 +0530

    commit

--- script.js
+++ script.js
@@ -30,23 +30,36 @@ Create Cluster Layer
 const styleCache = {};
 let cluster_layer = new ol.layer.Vector({ 
   source: new ol.source.Cluster({
-    distance: 100, 
-    minDistance: 100,
+    distance: 35, 
     source: new ol.source.Vector({
       features: features,
       projection:map.getView().projection
-    })    
-  }),
+    })
+  }),  
   style: function (feature) {
     const size = feature.get('features').length;
+    if (size == 1) {
+      return new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 5,
+          stroke: new ol.style.Stroke({
+            color: [255, 255, 255, 1],
+            width: 1,
+          }),
+          fill: new ol.style.Fill({
+            color: [23, 153, 54, 1],
+          }),
+        })
+      });
+    }
     let style = styleCache[size];
     if (!style) {
       style = new ol.style.Style({
         image: new ol.style.Circle({
-          radius: 30,
+          radius: 10,
           stroke: new ol.style.Stroke({
             color: [255, 255, 255, 0.7],
-            width: 10,
+            width: 5,
           }),
           fill: new ol.style.Fill({
             color: [0, 153, 255, 0.7],
@@ -54,7 +67,7 @@ let cluster_layer = new ol.layer.Vector({
         }),
         text: new ol.style.Text({
           text: size.toString(),
-          font: 'bold 20px serif',
+          font: 'bold 12px serif',
           fill: new ol.style.Fill({
             color: [0, 0, 0, 1],
           }),
@@ -78,15 +91,15 @@ let parser = new jsts.io.OL3Parser();
 // Display extent for each cluster on mouse over
 map.on("pointermove", (e) => {
   highlight_layer.getSource().clear();
-  cluster_layer.getFeatures(e.pixel).then((hoverFeatures) => {
+  cluster_layer.getFeatures(e.pixel).then((hoverFeatures) => {    
     if (hoverFeatures.length) {
       // Get clustered Coordinates
       const features = hoverFeatures[0].get('features');
-      if (features.length > 1) {        
-        let geom_points = new ol.geom.MultiPoint(features.map((r) => r.getGeometry().getCoordinates()));
-        let geom_points_convexhull = parser.write(parser.read(geom_points).convexHull());
-        let cluster_polygon = new ol.Feature(geom_points_convexhull);       
-        
+      if (features.length > 1) {
+        let geom_points = new ol.geom.MultiPoint(features.map((r) => r.getGeometry().getCoordinates()));        
+        let convexHull = geom_points.convexHull();
+        convexHull.push(convexHull[0]);
+        let cluster_polygon = new ol.Feature(new ol.geom.Polygon([convexHull]));
         highlight_layer.getSource().addFeature(cluster_polygon);
       }
     }
